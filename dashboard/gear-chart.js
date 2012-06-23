3a1031cb9ef9276a03b203d4cd4c7776b31f3cf0
Boris Smus <boris@smus.com>
2012-06-23 13:13:00 -0700

    Added gear visualization too.

--- dashboard/gear-chart.js
+++ dashboard/gear-chart.js
@@ -1,64 +1,134 @@
 (function(exports) {
 
-function GearChart(options) {
+function AngleChart(options) {
+  this.angle = 0;
   this._init();
 }
 
-GearChart.prototype.setValue = function(value) {
+AngleChart.prototype.setValue = function(value) {
 };
 
-GearChart.prototype._init = function() {
+AngleChart.prototype._init = function() {
   // Draw a circle.
   // Draw an arrow pointing in the current direction.
   // Keep a handle to the arrow object because we're going to be moving it
   // around.
-  var margin = {top: 6, right: 0, bottom: 6, left: 40},
-      width = 960 - margin.right,
-      height = 120 - margin.top - margin.bottom;
+  var svg = d3.select("body").insert("svg:svg", "form")
+    .attr("width", w)
+    .attr("height", h)
+  .append("svg:g")
+    .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")scale(.9)")
+  .append("svg:g")
+    .data([{radius: r * 5}]);
 
-  var x = d3.scale.linear()
-      .domain(this.domain)
-      .range([0, width]);
+svg.append("svg:g")
+    .attr("class", "ring")
+    .data([{teeth: 80, radius: -r * 5, ring: true}])
+  .append("svg:path")
+    .attr("class", "gear")
+    .attr("d", gear);
 
-  var y = d3.scale.linear()
-      .domain(this.range)
-      .range([height, 0]);
+var sun = svg.append("svg:g")
+    .attr("class", "sun")
+    .data([{teeth: 16, radius: r}])
+  .append("svg:g")
+    .attr("class", "gear");
 
-  var line = d3.svg.line()
-      .interpolate(this.interpolation)
-      .x(function(d, i) { return x(i); })
-      .y(function(d, i) { return y(d); });
+sun.append("svg:path")
+    .attr("d", gear);
 
-  var svg = d3.select("body").append("p").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+sun.append("svg:text")
+    .attr("x", r / 2 + 6)
+    .attr("dy", ".31em")
+    .attr("text-anchor", "middle")
+    .text("D3");
 
-  svg.append("defs").append("clipPath")
-      .attr("id", "clip")
-    .append("rect")
-      .attr("width", width)
-      .attr("height", height);
+var planet1 = svg.append("svg:g")
+    .attr("class", "planet")
+    .attr("transform", "translate(0,-" + r * 3 + ")")
+    .data([{teeth: 32, radius: -r * 2}])
+  .append("svg:g")
+    .attr("class", "gear");
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(d3.svg.axis().scale(y).ticks(5).orient("left"));
+planet1.append("svg:path")
+    .attr("d", gear);
 
-  var path = svg.append("g")
-      .attr("clip-path", "url(#clip)")
-    .append("path")
-      .data([this.data])
-      .attr("class", "line")
-      .attr('stroke', 'black')
-      .attr('fill', 'none')
-      .attr('stroke-width', 2)
-      .attr("d", line);
+planet1.append("svg:text")
+    .attr("x", -r)
+    .attr("dy", ".31em")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(45)")
+    .text("CSS");
 
-  this.path = path;
-  this.line = line;
-  this.x = x;
+var planet2 = svg.append("svg:g")
+    .attr("class", "planet")
+    .attr("transform", "translate(" + -r * 3 * x + "," + -r * 3 * y + ")")
+    .data([{teeth: 32, radius: -r * 2}])
+  .append("svg:g")
+    .attr("class", "gear");
+
+planet2.append("svg:path")
+    .attr("d", gear);
+
+planet2.append("svg:text")
+    .attr("x", -r)
+    .attr("dy", ".31em")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(45)")
+    .text("HTML");
+
+var planet3 = svg.append("svg:g")
+    .attr("class", "planet")
+    .attr("transform", "translate(" + r * 3 * x + "," + -r * 3 * y + ")")
+    .data([{teeth: 32, radius: -r * 2}])
+  .append("svg:g")
+    .attr("class", "gear");
+
+planet3.append("svg:path")
+    .attr("d", gear);
+
+planet3.append("svg:text")
+    .attr("x", -r)
+    .attr("dy", ".31em")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(45)")
+    .text("SVG");
+
+d3.selectAll("input[name=reference]")
+    .data([r * 5, Infinity, -r])
+    .on("change", function(d) { svg.data([{radius: d}]); });
+
+d3.selectAll("input[name=speed]")
+    .on("change", function() { speed = +this.value; });
+
+function gear(d) {
+  var n = d.teeth,
+      r2 = Math.abs(d.radius),
+      r0 = r2 - 8,
+      r1 = r2 + 8,
+      r3 = d.ring ? (r3 = r0, r0 = r1, r1 = r3, r2 + 20) : 20,
+      da = Math.PI / n,
+      a0 = -Math.PI / 2 + (d.ring ? Math.PI / n : 0),
+      i = -1,
+      path = ["M", r0 * Math.cos(a0), ",", r0 * Math.sin(a0)];
+  while (++i < n) path.push(
+      "A", r0, ",", r0, " 0 0,1 ", r0 * Math.cos(a0 += da), ",", r0 * Math.sin(a0),
+      "L", r2 * Math.cos(a0), ",", r2 * Math.sin(a0),
+      "L", r1 * Math.cos(a0 += da / 3), ",", r1 * Math.sin(a0),
+      "A", r1, ",", r1, " 0 0,1 ", r1 * Math.cos(a0 += da / 3), ",", r1 * Math.sin(a0),
+      "L", r2 * Math.cos(a0 += da / 3), ",", r2 * Math.sin(a0),
+      "L", r0 * Math.cos(a0), ",", r0 * Math.sin(a0));
+  path.push("M0,", -r3, "A", r3, ",", r3, " 0 0,0 0,", r3, "A", r3, ",", r3, " 0 0,0 0,", -r3, "Z");
+  return path.join("");
+}
+
+d3.timer(function() {
+  var angle = (Date.now() - start) * speed,
+      transform = function(d) { return "rotate(" + angle / d.radius + ")"; };
+  svg.selectAll(".gear").attr("transform", transform);
+  svg.attr("transform", transform); // fixed ring
+});
 };
 
-exports.GearChart = GearChart;
+exports.AngleChart = AngleChart;
 })(window);

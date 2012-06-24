af802c978c7887de5ad6aa3736e9e8a475963162
Renato Mangini <mangini@gmail.com>
2012-06-23 17:22:29 -0700

    moved code to the new UI

--- app/background.js
+++ app/background.js
@@ -1,3 +1,7 @@
 chrome.experimental.app.onLaunched.addListener(function() {
   chrome.appWindow.create("static/index.html", {width:1024, height: 768});
 });
+
+chrome.experimental.app.onLaunched.addListener(function() {
+  chrome.appWindow.create('static/nxt.html', {width: 343, height: 600});
+});
--- nxt-app/js/background.js
+++
@@ -1,5 +0,0 @@
-
-chrome.experimental.app.onLaunched.addListener(function() {
-  chrome.appWindow.create('nxt.html', 
-     {frame: 'custom', width: 343, height: 600});
-});

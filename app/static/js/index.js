327504ae8538a3d22e04467e13d3169b12e81610
Boris Smus <borissmus@Boriss-MacBook-Pro.local>
2012-06-24 12:33:09 -0700

    Adding Front screen for pre-flight checks..... and changes to the API

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,23 +1,63 @@
 window.addEventListener("load", function() {
   var panel = document.getElementById("panel");
+  var status = document.getElementById("status");
+  var robotSelect = document.getElementById("devices");
   var _x=0;
   var _y=0;
   var _arm=0;
 
-  var controller = new Controller();  
-  controller.OnVertical = function(x) {
+  var state = {robot: false, gamepad: false};
+
+  var updateState = function(name, val) {
+    state[name] = val;
+
+    if(state["robot"] && state["gamepad"]) {
+      status.classList.add("ok");
+    }
+    else {
+      status.classList.remove("ok");    
+    }
+  };
+
+  robotSelect.addEventListener("change", function() {
+    robot.connect(robotSelect.value, function() {
+      // Success
+      var robotEl = document.querySelector(".robot.status");
+      robotEl.classList.add("found");
+      updateState("robot", true);
+    });
+  });
+
+  var params = {};
+   
+  params.OnVertical = function(x) {
     _x = x;
     panel.style.webkitTransform = "rotate3d(1, 0, 0, " + _x/10 + "deg) rotate3d(0, 1, 0, " + _y/10 + "deg)";
     robot.move(_x, _y); 
   };
 
-  controller.OnHorizontal = function(y) {
+  params.OnHorizontal = function(y) {
     _y = y;
     panel.style.webkitTransform = "rotate3d(1, 0, 0, " + _x/10 + "deg) rotate3d(0, 1, 0, " + _y/10 + "deg)";
     robot.move(_x, _y); 
   };
 
-  controller.OnArm = function(arm) {
+  params.OnArm = function(arm) {
     robot.moveArm(arm);
   };
+
+  params.OnDeviceFound = function(name) {
+    var status = document.querySelector(".gamepad.status");
+    status.classList.add("found"); 
+    updateState("gamepad", true);
+  };
+
+  params.OnDeviceLost = function(name) {
+    var status = document.querySelector(".gamepad.status");
+    status.classList.remove("found"); 
+    updateState("gamepad", false);
+  }
+
+  var controller = new Controller(params);
+  
 }, false);
